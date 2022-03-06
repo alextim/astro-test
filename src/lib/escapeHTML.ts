060464970fc5e09803b0c0cd19e3b6094331b4a8
Alex Tim <buildmnt@gmail.com>
2022-03-06 22:02:16 +0200

    image

--- src/lib/escapeHTML.ts
+++ src/lib/escapeHTML.ts
@@ -2,17 +2,7 @@ const escapeHTML = (s: any): string => {
   if (!s) {
     return '';
   }
-  return s.replace(
-    /[&<>'"]/g,
-    tag =>
-    ({
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      "'": '&#39;',
-      '"': '&quot;'
-    }[tag] || tag)
-  );
+  return s.replace(/[&<>'"]/g, (tag) => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', "'": '&#39;', '"': '&quot;' }[tag] || tag));
 };
 
 export default escapeHTML;
